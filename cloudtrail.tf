0101024ce7ade97db155a7ee7566a28d806a6226
Ritik Gupta <ritikgupta3@kpmg.com>
2024-04-05 13:38:12 +0530

    test cloudtrail

---
+++ cloudtrail.tf
@@ -0,0 +1,60 @@
+module "test" {
+  source                        = "./cloudtrail"
+  name                          = "test"
+  s3_bucket_name                = aws_s3_bucket.example.id
+  enable_log_file_validation    = true
+  is_multi_region_trail         = false
+  include_global_service_events = false
+  enable_logging                = true
+  is_organization_trail         = false
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket        = "abcdnikhil"
+  force_destroy = true
+}
+
+data "aws_iam_policy_document" "example" {
+  statement {
+    sid    = "AWSCloudTrailAclCheck"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:GetBucketAcl"]
+    resources = [aws_s3_bucket.example.arn]
+  }
+
+  statement {
+    sid    = "AWSCloudTrailWrite"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com", "config.amazonaws.com"]
+    }
+
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.example.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "example" {
+  bucket = aws_s3_bucket.example.id
+  policy = data.aws_iam_policy_document.example.json
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_partition" "current" {}
+
+data "aws_region" "current" {}
