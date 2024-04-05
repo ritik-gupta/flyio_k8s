4d6bd03e75efcefb2c41076c403c791c98dc972e
Ritik Gupta <ritikgupta3@kpmg.com>
2024-04-05 11:53:01 +0530

    comment out everythign

--- eks.tf
+++ eks.tf
@@ -1,27 +1,27 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.3"
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "19.15.3"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.29"
+#   cluster_name    = local.cluster_name
+#   cluster_version = "1.29"
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
+#   vpc_id                         = module.vpc.vpc_id
+#   subnet_ids                     = module.vpc.private_subnets
+#   cluster_endpoint_public_access = true
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-  }
+#   eks_managed_node_group_defaults = {
+#     ami_type = "AL2_x86_64"
+#   }
 
-  eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
+#   eks_managed_node_groups = {
+#     one = {
+#       name = "node-group-1"
 
-      instance_types = ["r6i.xlarge"]
+#       instance_types = ["r6i.xlarge"]
 
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-    }
-  }
-}
+#       min_size     = 1
+#       max_size     = 3
+#       desired_size = 2
+#     }
+#   }
+# }
