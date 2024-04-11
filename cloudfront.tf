0040686843f24b015735df6797468ff13470f66a
Ritik Gupta <ritikgupta3@kpmg.com>
2024-04-11 15:14:01 +0530

    Added missing values

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,187 +1,44 @@
-locals {
-  domain_name = "terraform-aws-modules.modules.tf" # trimsuffix(data.aws_route53_zone.this.name, ".")
-  subdomain   = "cdn"
-}
-
 module "cloudfront" {
   source  = "terraform-aws-modules/cloudfront/aws"
   version = "3.4.0"
 
-  aliases = ["${local.subdomain}.${local.domain_name}"]
+  aliases = ["malik.vc"]
 
-  comment             = "My awesome CloudFront"
   enabled             = true
   staging             = false # If you want to create a staging distribution, set this to true
   http_version        = "http2and3"
   is_ipv6_enabled     = true
   price_class         = "PriceClass_All"
   retain_on_delete    = false
-  wait_for_deployment = false
-
-  # If you want to create a primary distribution with a continuous deployment policy, set this to the ID of the policy.
-  # This argument should only be set on a production distribution.
-  # ref. `aws_cloudfront_continuous_deployment_policy` resource: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_continuous_deployment_policy
-  continuous_deployment_policy_id = null
-
-  # When you enable additional metrics for a distribution, CloudFront sends up to 8 metrics to CloudWatch in the US East (N. Virginia) Region.
-  # This rate is charged only once per month, per metric (up to 8 metrics per distribution).
-#   create_monitoring_subscription = true
-
-#   create_origin_access_identity = true
-#   origin_access_identities = {
-#     s3_bucket_one = "My awesome CloudFront can access"
-#   }
-
-#   create_origin_access_control = true
-#   origin_access_control = {
-#     s3_oac = {
-#       description      = "CloudFront access to S3"
-#       origin_type      = "s3"
-#       signing_behavior = "always"
-#       signing_protocol = "sigv4"
-#     }
-#   }
-
-#   logging_config = {
-#     bucket = module.log_bucket.s3_bucket_bucket_domain_name
-#     prefix = "cloudfront"
-#   }
+  wait_for_deployment = true
 
   origin = {
-    appsync = {
-      domain_name = "appsync.${local.domain_name}"
+    "ac46dbd1f56f341a5812d6514c8ff7df-303408738.eu-west-2.elb.amazonaws.com" = {
+      connection_attempts = 3
+      connection_timeout  = 10
+      origin_path         = "/ritik"
+      domain_name         = "ac46dbd1f56f341a5812d6514c8ff7df-303408738.eu-west-2.elb.amazonaws.com"
       custom_origin_config = {
-        http_port              = 80
-        https_port             = 443
-        origin_protocol_policy = "match-viewer"
-        origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+        http_port                = 80
+        https_port               = 443
+        origin_keepalive_timeout = 5
+        origin_protocol_policy   = "http-only"
+        origin_read_timeout      = 30
+        origin_ssl_protocols     = ["SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"]
       }
-
-      custom_header = [
-        {
-          name  = "X-Forwarded-Scheme"
-          value = "https"
-        },
-        {
-          name  = "X-Frame-Options"
-          value = "SAMEORIGIN"
-        }
-      ]
-
-      origin_shield = {
-        enabled              = true
-        origin_shield_region = "us-east-1"
-      }
-    }
-
-    # s3_one = { # with origin access identity (legacy)
-    #   domain_name = module.s3_one.s3_bucket_bucket_regional_domain_name
-    #   s3_origin_config = {
-    #     origin_access_identity = "s3_bucket_one" # key in `origin_access_identities`
-    #     # cloudfront_access_identity_path = "origin-access-identity/cloudfront/E5IGQAA1QO48Z" # external OAI resource
-    #   }
-    # }
-
-    # s3_oac = { # with origin access control settings (recommended)
-    #   domain_name           = module.s3_one.s3_bucket_bucket_regional_domain_name
-    #   origin_access_control = "s3_oac" # key in `origin_access_control`
-    #   #      origin_access_control_id = "E345SXM82MIOSU" # external OAС resource
-    # }
-  }
-
-  origin_group = {
-    group_one = {
-      failover_status_codes      = [403, 404, 500, 502]
-      primary_member_origin_id   = "appsync"
-      secondary_member_origin_id = "s3_one"
     }
   }
 
   default_cache_behavior = {
-    target_origin_id       = "appsync"
+    target_origin_id       = "ac46dbd1f56f341a5812d6514c8ff7df-303408738.eu-west-2.elb.amazonaws.com"
     viewer_protocol_policy = "allow-all"
-    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
 
     use_forwarded_values = false
-
-    cache_policy_id            = "b2884449-e4de-46a7-ac36-70bc7f1ddd6d"
-    response_headers_policy_id = "67f7725c-6f97-4210-82d7-5512b31e9d03"
-
-    # lambda_function_association = {
-
-    #   # Valid keys: viewer-request, origin-request, viewer-response, origin-response
-    #   viewer-request = {
-    #     lambda_arn   = module.lambda_function.lambda_function_qualified_arn
-    #     include_body = true
-    #   }
-
-    #   origin-request = {
-    #     lambda_arn = module.lambda_function.lambda_function_qualified_arn
-    #   }
-    # }
+    compress             = true
+    cache_policy_id      = "658327ea-f89d-4fab-a63d-7e88639e58f6"
   }
-
-  ordered_cache_behavior = [
-    {
-      path_pattern           = "/static/*"
-      target_origin_id       = "s3_one"
-      viewer_protocol_policy = "redirect-to-https"
-
-      allowed_methods = ["GET", "HEAD", "OPTIONS"]
-      cached_methods  = ["GET", "HEAD"]
-
-      use_forwarded_values = false
-
-      cache_policy_name            = "Managed-CachingOptimized"
-      origin_request_policy_name   = "Managed-UserAgentRefererHeaders"
-      response_headers_policy_name = "Managed-SimpleCORS"
-
-    #   function_association = {
-    #     # Valid keys: viewer-request, viewer-response
-    #     viewer-request = {
-    #       function_arn = aws_cloudfront_function.example.arn
-    #     }
-
-    #     viewer-response = {
-    #       function_arn = aws_cloudfront_function.example.arn
-    #     }
-    #   }
-    },
-    {
-      path_pattern           = "/static-no-policies/*"
-      target_origin_id       = "s3_one"
-      viewer_protocol_policy = "redirect-to-https"
-
-      allowed_methods = ["GET", "HEAD", "OPTIONS"]
-      cached_methods  = ["GET", "HEAD"]
-
-      # Using Cache/ResponseHeaders/OriginRequest policies is not allowed together with `compress` and `query_string` settings
-      compress     = true
-      query_string = true
-    }
-  ]
-
-#   viewer_certificate = {
-#     acm_certificate_arn = module.acm.acm_certificate_arn
-#     ssl_support_method  = "sni-only"
-#   }
-
-  custom_error_response = [{
-    error_code         = 404
-    response_code      = 404
-    response_page_path = "/errors/404.html"
-    }, {
-    error_code         = 403
-    response_code      = 403
-    response_page_path = "/errors/403.html"
-  }]
-
-  geo_restriction = {
-    restriction_type = "whitelist"
-    locations        = ["NO", "UA", "US", "GB"]
-  }
-
 }
 
 ######
