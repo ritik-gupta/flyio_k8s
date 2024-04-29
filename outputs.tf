888acdfc2a5c7c0aab480af7902346aaaa984f3c
Ritik Gupta <gritik95@gmail.com>
2024-04-29 20:45:04 +0530

    feat: ecs with fargate

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,3 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
+output "alb_hostname" {
+  value = aws_alb.main.dns_name
 }
-
-output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = module.eks.cluster_name
-}
-
-# output "acm_certificate_domain_validation_options" {
-#   value = aws_acm_certificate.this.domain_validation_options
-# }
-
-# output "cert_status" {
-#   value = aws_acm_certificate.this.status
-# }
---
+++ versions.tf
@@ -0,0 +1,17 @@
+terraform {
+
+#   cloud {
+#     organization = "supamakers"
+#     workspaces {
+#       name = "saasmaker-$main"
+#     }
+#   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.45.0"
+    }
+  }
+  required_version = "~> 1.3"
+}
