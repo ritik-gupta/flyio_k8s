f340946a61fefa76ab9e3cf3147e308a12b8d325
Ritik Gupta <gritik95@gmail.com>
2024-04-30 14:26:36 +0530

    test

--- alb.tf
+++ alb.tf
@@ -6,7 +6,7 @@ resource "aws_alb" "main" {
 
 resource "aws_alb_target_group" "app" {
   name        = "cb-target-group"
-  port        = 3000
+  port        = var.app_port
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
   target_type = "ip"
